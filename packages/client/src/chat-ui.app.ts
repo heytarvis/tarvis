361e7b7937872a3c7ff41073fa326f6a16d9c481
Tom Österlund <tom.osterlund1@gmail.com>
2025-06-20 11:45:22 +0200

    fix build

--- packages/client/src/chat-ui.app.ts
+++ packages/client/src/chat-ui.app.ts
@@ -5,27 +5,27 @@ import { CustomComponentFn, CustomComponentFns } from './types/app';
 import { ClientPlugin } from '@tarvis/shared/src/types/client-plugin';
 
 export class ChatUI {
-  #ctx: ChatUiContext;
-  #preactRoot: HTMLElement | null = null;
+  private readonly ctx: ChatUiContext;
+  private preactRoot: HTMLElement | null = null;
 
   constructor(ctx: ChatUiContext) {
-    this.#ctx = ctx;
+    this.ctx = ctx;
   }
 
   async render(el: HTMLElement): Promise<void> {
     await this.#beforeRender();
 
-    this.#preactRoot = document.createElement('div');
-    this.#preactRoot.className = 'tarvis__preact-root';
-    el.appendChild(this.#preactRoot);
-    render(createElement(ChatUIComponent, { ctx: this.#ctx }), this.#preactRoot);
+    this.preactRoot = document.createElement('div');
+    this.preactRoot.className = 'tarvis__preact-root';
+    el.appendChild(this.preactRoot);
+    render(createElement(ChatUIComponent, { ctx: this.ctx }), this.preactRoot);
   }
 
   destroy(): void {
-    if (this.#preactRoot) {
-      render(null, this.#preactRoot);
-      this.#preactRoot.remove();
-      this.#preactRoot = null;
+    if (this.preactRoot) {
+      render(null, this.preactRoot);
+      this.preactRoot.remove();
+      this.preactRoot = null;
     }
   }
 
@@ -33,13 +33,13 @@ export class ChatUI {
    * Internal API for framework adapter components, to set custom component functions.
    * */
   _setCustomComponentFn(fnId: keyof CustomComponentFns, fn: CustomComponentFn) {
-    this.#ctx.customComponents[fnId] = fn;
+    this.ctx.customComponents[fnId] = fn;
   }
 
   async #beforeRender() {
-    if (this.#ctx.plugins?.value && this.#ctx.plugins.value.length > 0) {
+    if (this.ctx.plugins?.value && this.ctx.plugins.value.length > 0) {
       await Promise.all(
-        this.#ctx.plugins.value.map((plugin: ClientPlugin) => plugin.beforeRender?.(this.#ctx))
+        this.ctx.plugins.value.map((plugin: ClientPlugin) => plugin.beforeRender?.(this.ctx))
       );
     }
   }
